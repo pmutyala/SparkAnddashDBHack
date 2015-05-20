955c9ef26f7c1f184c7ad3186c1fa984f7ddea94
Pandu Ranga Rao Mutyala <pandumutyala@hotmail.com>
2015-05-20 13:13:28 -0400

    Update SparkAnddashDB_Pandas_PySpark_IPython_Demo.py

--- SparkAnddashDB_Pandas_PySpark_IPython_Demo.py
+++ SparkAnddashDB_Pandas_PySpark_IPython_Demo.py
@@ -1,7 +1,8 @@
 import pyodbc as dashdb
 import pandas as pd
-import numpy
+import numpy as np
 import matplotlib as mlib
+import matplotlib.pyplot as plt
 cnx = dashdb.connect("DRIVER={IBM DB2 ODBC DRIVER};DSN=BLUDB;UID=<USERNAME>;PWD=<PASSWORD>") # username and password from connection settings
 cur = cnx.cursor()
 sql = "select * from collisions"
