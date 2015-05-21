fc1278f35d0d68d5bcc2d77c0212a3deac7fce8f
Pandu Ranga Rao Mutyala <pandumutyala@hotmail.com>
2015-05-21 15:03:17 -0400

    Update SparkAnddashDB_Pandas_PySpark_IPython_Demo.py

--- SparkAnddashDB_Pandas_PySpark_IPython_Demo.py
+++ SparkAnddashDB_Pandas_PySpark_IPython_Demo.py
@@ -24,6 +24,11 @@ df1 = pd.read_sql(sql1, cnx)
 sql2 = "select BOROUGH,sum(NUMBER_OF_PERSONS_KILLED) as persons_killed, sum(NUMBER_OF_CYCLIST_KILLED) as cyclists_killed,sum(NUMBER_OF_PEDESTRIANS_KILLED) as pedestrians_KILLED,sum(NUMBER_OF_MOTORIST_KILLED) as motorist_killed from collisions where BOROUGH <> \'\' group by BOROUGH"
 df2 = pd.read_sql(sql2, cnx)
 df3 = pd.merge(df1,df2)
+# from spark, loading json for incoming data
+df_spark =  sqlContext.jsonFile("/home/pmutyala/collisions/collisions.json")
+df_spark.registerAsTable("df_spark")
+df_spark_sql1 = sqlContext.sql("select BOROUGH,sum(cast(NUMBER_OF_PERSONS_KILLED as INT)) as PERSONS_KILLED, sum(cast(NUMBER_OF_CYCLIST_KILLED as INT)) as CYCLISTS_KILLED,sum(cast(NUMBER_OF_PEDESTRIANS_KILLED as INT)) as PEDESTRIANS_KILLED,sum(cast(NUMBER_OF_MOTORIST_KILLED as INT)) as MOTORIST_KILLED from df_spark where BOROUGH <> \'\' group by BOROUGH")
+df_spark_sql2 = sqlContext.sql("select BOROUGH,sum(cast(NUMBER_OF_PERSONS_INJURED as INT)) as PERSONS_INJURED, sum(cast(NUMBER_OF_CYCLIST_INJURED as INT)) as CYCLISTS_INJURED,sum(cast(NUMBER_OF_PEDESTRIANS_INJURED as INT)) as PEDESTRIANS_INJURED,sum(cast(NUMBER_OF_MOTORIST_INJURED as INT)) as MOTORIST_INJURED from df_spark where BOROUGH <> \'\' group by BOROUGH")
 plt.figure()
 stack_plot = df3.plot(kind='bar',stacked=True,title="Number of Persons Injured or Killed in a Borough",label=df3.BOROUGH)
 stack_plot.legend(bbox_to_anchor=(1.6, 0.85))
